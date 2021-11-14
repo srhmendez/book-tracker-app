77a60bef647680d8190d1e049e5837845d7800e2
Sarah Mendez <srhmendez@gmail.com>
2021-11-14 15:22:25 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const bodyParser = require ('body-parser');
+const router = require ('./routes/router');
+
+const PORT = 8000;
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use('/', router);
+
+
+// connectDB();
+
+app.get('/', (req, res) => {
+    res.send('Hello World :)')
+})
+
+app.listen(PORT, () => console.log(`YAY server running on http://localhost:${PORT}`));         
+
+module.exports = app;
