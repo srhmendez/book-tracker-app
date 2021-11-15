fd0c34e6dbf0fa7f663166c39568690eb07c64bb
Sarah Mendez <srhmendez@gmail.com>
2021-11-14 21:53:49 -0700

    testing

--- connection/connection.js
+++ connection/connection.js
@@ -6,7 +6,7 @@ console.log(process.env.MONGO_URI)
 const connectDB = async () => {
     try {
         //connecting to mongoDB : String
-        const con = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+        const con = await mongoose.connect(process.env.MONGO_URI)
         console.log(`Mongo DB has successfully connected : ${con.connection.host}`)
 
     } catch (error) {
