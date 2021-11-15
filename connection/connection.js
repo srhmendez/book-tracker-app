e5a94051604f2c7db238903d8e2baa0f3065cb5b
Sarah Mendez <srhmendez@gmail.com>
2021-11-14 17:34:47 -0700

    messing with config var settings

--- connection/connection.js
+++ connection/connection.js
@@ -7,7 +7,6 @@ const connectDB = async () => {
     try {
         //connecting to mongoDB : String
         const con = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
-
         console.log(`Mongo DB has successfully connected : ${con.connection.host}`)
 
     } catch (error) {
