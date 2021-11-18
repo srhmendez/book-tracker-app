e3ac95884cd12dff524fa7d758f3a99785c56819
Sarah Mendez <srhmendez@gmail.com>
2021-11-18 15:55:37 -0700

    removing uri var in connection fn

--- connection/connection.js
+++ connection/connection.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config({ path: './config.env' });
-const URI = process.env.MONGO_URI
+
 
 const connectDB = async () => {
     try {
         //connecting to mongoDB : String
-        const con = await mongoose.connect(URI, {
+        const con = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
+            useUnifiedTopology: true
         })
         console.log(`Mongo DB has successfully connected : ${con.connection.host}`)
 
